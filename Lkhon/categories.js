8a1851ca2c8ac343844512ce9874d688770bfd9b
leakkk <leak@Kimly.local>
2019-10-20 22:41:52 +0700

    done

--- Lkhon/categories.js
+++ Lkhon/categories.js
@@ -282,3 +282,18 @@ var globalController = (function(cardCtrl, UICtrl) {
 })(cardController, UIController);
 
 globalController.init();
+
+// Card interation
+
+const cards = document.querySelectorAll(".card");
+
+function transition(event) {
+  if (this.classList.contains("active")) {
+    if (event.target.tagName != "H4" && event.target.className != "btn")
+      this.classList.remove("active");
+  } else {
+    this.classList.add("active");
+  }
+}
+
+cards.forEach(card => card.addEventListener("click", transition));
